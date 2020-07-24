e50464feb6515a3b37be7e789cb2ced465c34a4e
Alexandre Fischer <alexandre.fischer@orchestra.eu>
2020-07-24 16:40:13 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function index(number) {
+  if (!number) return
+
+  const lastIndexDot = number.lastIndexOf(".")
+  const lastIndexComma = number.lastIndexOf(",")
+
+  number = number.trim().replace(/\s/g, "")
+
+  if (lastIndexComma > lastIndexDot) {
+    number = number.replace(/\./g, "")
+    number = number.replace(/\,/g, ".")
+  } else {
+    number = number.replace(/\,/g, "")
+  }
+  number = number.replace(/[^0-9\.]+/g, "")
+  return parseFloat(number)
+}
+
+export default index;
